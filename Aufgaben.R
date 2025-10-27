4a513fc2ff0f364d23b514ee98d0d3f134400da2
Letsudem <sergesletsudem@gmail.com>
2025-10-27 22:59:48 +0100

    some change in the data

---
+++ Aufgaben.R
@@ -0,0 +1,4 @@
+install.packages(c("data.table","dplyr","tidyr","ggplot2","lubridate",
+                   "DBI","RSQLite","broom","tweedie","statmod"))
+library(data.table); library(dplyr); library(tidyr); library(ggplot2)
+library(lubridate); library(DBI); library(broom); library(tweedie); library(statmod)
